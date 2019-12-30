c238f95beebeda597d290e55887593d3f30db127
cl4pper <cl4pper@gmail.com>
2019-12-29 23:38:06 -0300

    ADD: edit method to get all items from wardrobe

--- modules/wardrobe.js
+++ modules/wardrobe.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+// SCHEMA
+const Item = require('../models/item')
+
 // GETTING ALL
-router.get('/', (req, res) => {
-    res.send('Getting All')
+router.get('/', async (req, res) => {
+    try {
+        const items = await Item.find()
+        res.json(items)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 // CREATING ONE
