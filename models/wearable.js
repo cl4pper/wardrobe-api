2b270945cbf8a0fa6d7d9ea957863c21543af990
cl4pper <cl4pper@gmail.com>
2020-01-09 23:59:58 -0300

    feat: create model for items types

---
+++ models/wearable.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const wearableSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        minlength: 1
+    }
+})
+
+module.exports = mongoose.model('Wearables', wearableSchema)
